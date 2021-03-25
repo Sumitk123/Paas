0c727a3a3370d506bfcb97185203c8f37184eee6
Sumitk123 <81374480+Sumitk123@users.noreply.github.com>
2021-03-25 20:46:18 +0530

    Add files via upload

---
+++ bitwise operator.py
@@ -0,0 +1,12 @@
+def bitOper():
+    print("sagar panchal \nRoll no:T094")
+    a=int(input("Enter the value of a: " ))
+    b=int(input("Enter the value of b: "))
+    print("vaule of AND operator on a and b is = ", a&b)
+    print("vaule of OR operator on a and b is = ", a|b)
+    print("vaule of XOR operator on a and b is = ", a^b)
+    print("vaule of NOT operator on a  is = ", ~a)
+    print("vaule of LEFT SWIFT operator on a is = ", a<<1)
+    print("vaule of RIGHT SWIFT operator on b is = ", b>>1)
+bitOper()
+    
